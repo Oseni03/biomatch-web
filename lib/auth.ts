6f0c8013a95eb1be429a80f438c0b425c84cd790
Oseni03 <oseniayomide57@gmail.com>
2026-06-23 15:53:45 +0100

    feat: Add BetterAuth configuration

---
+++ lib/auth.ts
@@ -0,0 +1,26 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./prisma";
+
+export const auth = betterAuth({
+	database: prismaAdapter(prisma, {
+		provider: "postgresql", // or "mysql", "postgresql", ...etc
+	}),
+	user: {
+		additionalFields: {
+			genotype: {
+				type: "string",
+				required: false,
+				input: false,
+			},
+			role: {
+				type: "string",
+				required: true,
+				input: true,
+			},
+		},
+	},
+	emailAndPassword: {
+		enabled: true,
+	},
+});
